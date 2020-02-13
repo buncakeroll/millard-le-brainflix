146bcfca7fd157b6a8d6fbf288f07f00c51b2210
Millard <millard@millardle.com>
2020-02-13 01:02:27 -0500

    created react app boiler plate, assets, sass files, etc

---
+++ sprint-1/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+const Hello = () => {
+  return (
+    <h1>hello</h1>
+  )
+}
+
+function App() {
+  return (
+    <Hello />
+  );
+}
+
+export default App;
